bce93e69b64ceddb2dacb3cd3aab7abe7957a0c7
Jan-Hendrik Popp <j.hendrik.popp@gmail.com>
2016-10-24 15:40:48 +0200

    - uebung1.2

---
+++ src/uebung1/Main.java
@@ -0,0 +1,12 @@
+package uebung1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args.length != 0) {
+            System.out.println("Hello World: " + args[0]);
+        } else {
+            System.out.println("No argument present.");
+        }
+    }
+}
